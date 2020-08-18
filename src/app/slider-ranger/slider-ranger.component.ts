7b728ab0189700f8a41c732ceeea2c04cc751667
Rafael Carvalho <rapha.pse@outlook.com>
2020-08-18 15:31:10 -0300

    Update slider-ranger.component.ts

--- src/app/slider-ranger/slider-ranger.component.ts
+++ src/app/slider-ranger/slider-ranger.component.ts
@@ -8,67 +8,32 @@ import * as moment from 'moment';
   styleUrls: ['./slider-ranger.component.scss']
 })
 export class SliderRangerComponent implements OnInit {
-  public date;
+ public date;
   public localeDates: any;
-  public calender;
-  public diaPrimeiro = null;
+  public primaryDay = null;
+  public increment = 0;
 
-  public meses = [];
+  public months = [];
+  public indexClick: number;
 
+  public initialDay = new Date().getDate();
+  public month = new Date().getMonth();
+  public listWeek = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+  public listMonth = [
+    'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio',
+    'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
 
   ngOnInit(): void {
-    this.time();
+    this.timePicker();
   }
 
+  public timePicker(index?: number) {
 
-  newArray(length: number): any [] {
-    if (length >= 0) {
-      return new Array(length);
-    }
-  }
-
-
-
-
-
-
-  public time() {
-    moment.defineLocale('PT-BR', {
-      months : 'Janeiro_Fevereiro_Março_Abril_Maio_Junho_Julho_Agosto_Setembro_Outubro_Novembro_Dezembro'.split('_'),
-      monthsShort : 'jan_fev_mar_abr_mai_jun_jul_ago_set_out_nov_dez'.split('_'),
-      weekdays : 'Domingo_Segunda-feira_Terça-feira_Quarta-feira_Quinta-feira_Sexta-feira_Sábado'.split('_'),
-      weekdaysShort : 'Dom_Seg_Ter_Qua_Qui_Sex_Sáb'.split('_'),
-      weekdaysMin : 'D_S_T_Q_Q_S_S'.split('_'),
-      // calendar : {
-      //   sameDay: '[Hoje às] LT',
-      //   nextDay: '[Amanhã às] LT',
-      //   nextWeek: 'dddd [às] LT',
-      //   lastDay: '[Ontem às] LT',
-      //   sameElse: 'L'
-      // },
-      // relativeTime : {
-      //   future : 'em %s',
-      //   past : '%s atrás',
-      //   s : 'segundos',
-      //   m : 'um minuto',
-      //   mm : '%d minutos',
-      //   h : 'uma hora',
-      //   hh : '%d horas',
-      //   d : 'um dia',
-      //   dd : '%d dias',
-      //   M : 'um mês',
-      //   MM : '%d meses',
-      //   y : 'um ano',
-      //   yy : '%d anos'
-      // },
-    });
-    // QTD DIAS NO MES
-    this.date = moment('2020-02-02').locale('PT-BR');
-    this.localeDates = this.date._locale;
-
+    this.momentStart();
+    this.months = [];
 
     // quantidade de dias do mes corrente
-    this.calender = this.dayMonth(new Date().getMonth(), new Date().getFullYear());
+    // this.calender = this.dayMonth(new Date().getMonth(), new Date().getFullYear());
 
     // mostra o dia da semana - SEGUNDA OU TER ...
     // const indexWeek = String(new Date().getDay().toString()).split('/').reverse();
@@ -78,29 +43,49 @@ export class SliderRangerComponent implements OnInit {
     // for (let i = Number(new Date().getDate()); i >= 1; i--) {
     //   this.localeDates._weekdaysMin.forEach((x: string) => this.objectDate.dia = x);
     // }
-    const ano = new Date().getFullYear();
-    const mes = new Date().getMonth() + 1;
+    const year = new Date().getFullYear();
+    const month = (new Date().getMonth() + 1) + (index ? index : 0);
 
-    this.diaPrimeiro = new Date(ano, mes - 1, 1).getDay();
-    // this.objectDate.semana = this.diaPrimeiro + 1;
-    const totalDias = new Date(ano, mes, 0).getDate();
-    let inicio = this.diaPrimeiro;
 
-    for (let diaMes = 0; diaMes < totalDias;) {
-      let semana = [];
-      for (let diaSemana = inicio; diaSemana < 7 && diaMes < totalDias; diaSemana++) {
-        semana[diaSemana] = diaMes + 1;
-        diaMes++;
+    this.primaryDay = new Date(year, month - 1, 1).getDay();
+    const totalDays = new Date(year, month, 0).getDate();
+    let initial = this.primaryDay;
+    for (let day = 0; day < totalDays;) {
+      const week = [];
+      for (let i = initial; i < 7 && day < totalDays; i++) {
+        week[i] = day + 1;
+        day++;
       }
-      this.meses.push(semana);
-      inicio = 0;
+      this.months.push(week);
+      initial = 0;
     }
   }
 
+  private momentStart() {
+    moment.defineLocale('PT-BR', {
+      months : 'Janeiro_Fevereiro_Março_Abril_Maio_Junho_Julho_Agosto_Setembro_Outubro_Novembro_Dezembro'.split('_'),
+      monthsShort : 'jan_fev_mar_abr_mai_jun_jul_ago_set_out_nov_dez'.split('_'),
+      weekdays : 'Domingo_Segunda-feira_Terça-feira_Quarta-feira_Quinta-feira_Sexta-feira_Sábado'.split('_'),
+      weekdaysShort : 'Dom_Seg_Ter_Qua_Qui_Sex_Sáb'.split('_'),
+      weekdaysMin : 'D_S_T_Q_Q_S_S'.split('_'),
+    });
+    // QTD DIAS NO MES
+    this.date = moment(new Date()).locale('PT-BR');
+    this.localeDates = this.date._locale;
+  }
 
-  private dayMonth(mes, ano) {
-    const data = new Date(ano, mes, 0);
-    return data.getDate();
+  public backMonth() {
+    this.increment -= 1;
+    this.timePicker(this.increment);
+  }
+
+  public nextMonth() {
+    this.increment += 1;
+    this.timePicker(this.increment);
+  }
+
+  public selectDay(indexDay: number) {
+    this.initialDay = indexDay;
   }
 
 }
